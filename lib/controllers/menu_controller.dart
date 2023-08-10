a10f51ff5e30aee3c6341c8f6617eb794be48a7d
Xebastian <ernesty533k@gmail.com>
2023-08-10 16:47:10 +0200

    Added menu controller

---
+++ lib/controllers/menu_controller.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:templ/constants/style.dart';
+import 'package:templ/routing/routes.dart';
+
+class MenuController extends GetxController {
+  static MenuController instance = Get.find();
+  var activeItem = AppRoutes.OverViewPageRoute.obs;
+  var hoverItem = "".obs;
+
+  // keeps track of active name item
+  changeActiveItemTo(String itemName) {
+    activeItem.value = itemName;
+  }
+
+  // onHover
+  onHover(String itemName) {
+    if (!isActive(itemName)) hoverItem.value = itemName;
+  }
+
+  isActive(String itemName) => activeItem.value = itemName;
+  isHovering(String itemName) => hoverItem.value = itemName;
+
+  Widget returnIconFor(String itemName) {
+    switch (itemName) {
+      case AppRoutes.OverViewPageRoute:
+        return _customIcon(Icons.trending_up_outlined, itemName);
+      case AppRoutes.DriversPageRoute:
+        return _customIcon(Icons.drive_eta, itemName);
+      case AppRoutes.ClientsPageRoute:
+        return _customIcon(Icons.people_alt, itemName);
+      case AppRoutes.AuthenticationPageRoute:
+        return _customIcon(Icons.exit_to_app_rounded, itemName);
+
+      default:
+        return _customIcon(Icons.exit_to_app_rounded, itemName);
+    }
+  }
+
+// when active an icon is bigger than the rest
+// onHover, itemColor should change
+  Widget _customIcon(IconData icon, String itemName) {
+    if (isActive(itemName)) return Icon(icon, size: 22, color: AppColors.dark);
+    return Icon(icon, color: isHovering(itemName) ? AppColors.dark : AppColors.lightGrey);
+  }
+}
---
+++ lib/routing/routes.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: constant_identifier_names
+
+///This is the Global base route of the app.
+///All routes are to be defined only in this file and from the rest a reference be made.
+class AppRoutes {
+  /// The routes corresponding to the side menu items
+  static const OverViewPageRoute = "Overview";
+  static const DriversPageRoute = "Drivers";
+  static const ClientsPageRoute = "Clients";
+  static const AuthenticationPageRoute = "Authentication";
+
+  List sideMenuItems = [
+    OverViewPageRoute,
+    DriversPageRoute,
+    ClientsPageRoute,
+    AuthenticationPageRoute,
+  ];
+}

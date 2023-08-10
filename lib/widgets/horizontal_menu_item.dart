3be2c0093a91419464aa8b609cebfd2069a4b584
Xebastian <ernesty533k@gmail.com>
2023-08-10 17:26:00 +0200

    Added horizontal menu

--- lib/widgets/horizontal_menu_item.dart
+++ lib/widgets/horizontal_menu_item.dart
@@ -18,43 +18,45 @@ class HorizontalMenuItem extends StatelessWidget {
       onHover: (hoverValue) {
         hoverValue ? AppControllers.menuController.onHover(itemName) : AppControllers.menuController.onHover("cannot hover");
       },
-      child: Obx(() => Container(
-            decoration: BoxDecoration(
-              color: AppControllers.menuController.isHovering(itemName) ? AppColors.lightGrey : AppColors.transparent,
-            ),
-            child: Row(
-              children: [
-                Visibility(
-                  visible: AppControllers.menuController.isHovering(itemName) || AppControllers.menuController.isActive(itemName),
-                  child: Container(
-                    width: 6,
-                    height: 40,
+      child: Obx(
+        () => Container(
+          decoration: BoxDecoration(
+            color: AppControllers.menuController.isHovering(itemName) ? AppColors.lightGrey : AppColors.transparent,
+          ),
+          child: Row(
+            children: [
+              Visibility(
+                visible: AppControllers.menuController.isHovering(itemName) || AppControllers.menuController.isActive(itemName),
+                child: Container(
+                  width: 6,
+                  height: 40,
+                  color: AppColors.dark,
+                ),
+              ),
+              SizedBox(width: width / 80),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: AppControllers.menuController.returnIconFor(itemName),
+              ),
+              if (AppControllers.menuController.isActive(itemName))
+                Flexible(
+                  child: CustomText(
+                    text: itemName,
+                    color: AppControllers.menuController.isHovering(itemName) ? AppColors.dark : AppColors.lightGrey,
+                  ),
+                )
+              else
+                Flexible(
+                  child: CustomText(
+                    text: itemName,
                     color: AppColors.dark,
+                    size: 18,
                   ),
                 ),
-                SizedBox(width: width / 80),
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: AppControllers.menuController.returnIconFor(itemName),
-                ),
-                if (AppControllers.menuController.isActive(itemName))
-                  Flexible(
-                    child: CustomText(
-                      text: itemName,
-                      color: AppControllers.menuController.isHovering(itemName) ? AppColors.dark : AppColors.lightGrey,
-                    ),
-                  )
-                else
-                  Flexible(
-                    child: CustomText(
-                      text: itemName,
-                      color: AppColors.dark,
-                      size: 18,
-                    ),
-                  ),
-              ],
-            ),
-          )),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

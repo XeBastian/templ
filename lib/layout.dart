501e68ecdc2bfad0aa13d311cf2d6c0d7fd1738c
Xebastian <ernesty533k@gmail.com>
2023-08-10 19:05:15 +0200

    Fixed layout tabs

--- lib/layout.dart
+++ lib/layout.dart
@@ -4,6 +4,8 @@ import 'package:templ/widgets/large_screen.dart';
 import 'package:templ/widgets/small_screen.dart';
 import 'package:templ/widgets/top_nav.dart';
 
+import 'widgets/side_menu.dart';
+
 class SiteLayout extends StatelessWidget {
   SiteLayout({super.key});
   final GlobalKey<ScaffoldState> scaffoldStateKey = GlobalKey();
@@ -12,7 +14,9 @@ class SiteLayout extends StatelessWidget {
     return Scaffold(
       appBar: topNavigationBar(context, scaffoldStateKey),
       key: scaffoldStateKey,
-      drawer: const Drawer(),
+      drawer: const Drawer(
+        child: SideMenu(),
+      ),
       body: const ResponsiveWidget(
         largeScreen: LargeScreen(),
         smallScreen: SmallScreen(),
